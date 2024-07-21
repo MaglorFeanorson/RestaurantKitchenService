163e6e5896fbd904058c60d55fac7cbadb529553
MaglorFeanorson <daniilrog17@gmail.com>
2024-07-21 13:42:45 +0300

    feat: add URLs for dish types, dishes, cooks, and dish detail views

--- restaurant_kitchen/urls.py
+++ restaurant_kitchen/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from restaurant_kitchen.views import index
+from restaurant_kitchen.views import (
+    index,
+    DishTypeListView,
+    DishListView,
+    CookListView,
+    dish_detail_view
+)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish/", DishListView.as_view(), name="dish-list"),
+    path("cook/", CookListView.as_view(), name="cook-list"),
+    path("dish/<int:pk>/", dish_detail_view, name="dish-detail"),
+
 ]
 
 app_name = "restaurant_kitchen"
