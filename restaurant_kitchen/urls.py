85b023ce5139791f81b928e5e3d1c8ea3eedfa95
MaglorFeanorson <daniilrog17@gmail.com>
2024-07-22 13:21:04 +0300

    feat: Add path for deleting DishType instances

--- restaurant_kitchen/urls.py
+++ restaurant_kitchen/urls.py
@@ -4,11 +4,12 @@ from restaurant_kitchen.views import (
     index,
     DishTypeListView,
     DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
     DishListView,
     CookListView,
     CookDetailView,
     DishDetailView,
-    DishTypeUpdateView,
 
 
 )
@@ -19,6 +20,7 @@ urlpatterns = [
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
     path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
     path("dish-types/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish-types/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
     path("dish/", DishListView.as_view(), name="dish-list"),
     path("cook/", CookListView.as_view(), name="cook-list"),
     path("cook/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
