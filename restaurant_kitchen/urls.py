e374e3ea60878bce02ea4a498dfec0ad036ac14e
MaglorFeanorson <daniilrog17@gmail.com>
2024-07-22 11:31:08 +0300

    feat: add path for creating dish types in urls.py

--- restaurant_kitchen/urls.py
+++ restaurant_kitchen/urls.py
@@ -1,19 +1,26 @@
-from django.urls import path
+from django.urls import path, include
 
 from restaurant_kitchen.views import (
     index,
     DishTypeListView,
+    DishTypeCreateView,
     DishListView,
     CookListView,
-    dish_detail_view
+    CookDetailView,
+    DishDetailView,
+
+
 )
 
 urlpatterns = [
     path("", index, name="index"),
+
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
     path("dish/", DishListView.as_view(), name="dish-list"),
     path("cook/", CookListView.as_view(), name="cook-list"),
-    path("dish/<int:pk>/", dish_detail_view, name="dish-detail"),
+    path("cook/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
 
 ]
 
