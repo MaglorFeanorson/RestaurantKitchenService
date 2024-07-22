a1abc4a452ed64d8ee0883d1a6a18493ff2f58e2
MaglorFeanorson <daniilrog17@gmail.com>
2024-07-22 19:44:45 +0300

    feat: Add forms for Cook creation, Dish management, and search functionality

---
+++ restaurant_kitchen/forms.py
@@ -0,0 +1,62 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from restaurant_kitchen.models import Cook, Dish
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + ("username", "first_name", "last_name", "years_of_experience", )
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Dish
+        fields = ["name", "description", "price", "dish_type", "cooks"]
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        ),
+    )
+
+class DishFilterForm(forms.Form):
+    name = forms.CharField(required=False, label="Name")
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by username"
+            }
+        ),
+    )
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        ),
+    )
