f41445c225de90d75cc3c62ded5ee64992ad1916
MaglorFeanorson <daniilrog17@gmail.com>
2024-07-23 13:09:59 +0300

    fix: corrected flake8 erors

--- restaurant_kitchen/admin.py
+++ restaurant_kitchen/admin.py
@@ -14,9 +14,18 @@ class DishAdmin(admin.ModelAdmin):
 @admin.register(Cook)
 class CookAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("years_of_experience", )
-    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("years_of_experience",)}),)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional info", {"fields": ("first_name", "last_name", "email", "years_of_experience")}),
+        ("Additional info",
+         {"fields": (
+             "first_name",
+             "last_name",
+             "email",
+             "years_of_experience"
+         )}
+         ),
     )
 
 
@@ -24,4 +33,3 @@ class CookAdmin(UserAdmin):
 class DishTypeAdmin(admin.ModelAdmin):
     list_display = ["name",]
     search_fields = ["name",]
-
