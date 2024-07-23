eeb8977873a6ddb814eda8482e804ae0cf7fc53b
MaglorFeanorson <daniilrog17@gmail.com>
2024-07-23 13:12:35 +0300

    fix: corrected flake8 erors

--- restaurant_kitchen/urls.py
+++ restaurant_kitchen/urls.py
@@ -16,26 +16,52 @@ from restaurant_kitchen.views import (
     DishCreateView,
     DishUpdateView,
     DishDeleteView,
-
 )
-
 urlpatterns = [
     path("", index, name="index"),
 
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
-    path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
-    path("dish-types/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
-    path("dish-types/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
+    path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create"
+    ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update"
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete"
+    ),
     path("dish/", DishListView.as_view(), name="dish-list"),
     path("cook/", CookListView.as_view(), name="cook-list"),
     path("cooks/create/", CookCreateView.as_view(), name="cook-create"),
-    path("cooks/<int:pk>/update/", CookUpdateView.as_view(), name="cook-update"),
-    path("cooks/<int:pk>/delete/", CookDeleteView.as_view(), name="cook-delete"),
+    path(
+        "cooks/<int:pk>/update/",
+        CookUpdateView.as_view(),
+        name="cook-update"
+    ),
+    path(
+        "cooks/<int:pk>/delete/",
+        CookDeleteView.as_view(),
+        name="cook-delete"
+    ),
     path("cook/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
     path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("dish/create/", DishCreateView.as_view(), name="dish-create"),
-    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
-    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
+    path(
+        "dish/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update"
+    ),
+    path(
+        "dish/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete"
+    ),
 ]
 
 app_name = "restaurant_kitchen"
